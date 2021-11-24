848f880dae776f6227acd2ecbb2c25a1ede3877f
Jose Gonzalez <jlgr1994@gmail.com>
2021-11-24 05:50:11 -0500

    Configuracion de nombre base de datos y creación de migracion para la tabla productos

---
+++ database/migrations/2021_11_24_095259_productos.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Productos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    /**
+     * Creación de funcion para crear migrción
+     * de tabla productos.
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('imagen');
+            $table->decimal('precio', 5, 2)->nullable()->default(123.45);
+            $table->string('descripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
